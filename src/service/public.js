453000251fed94fdb184f267efe61ea8c2494c7d
pc1117 <1023861544@qq.com>
2018-12-27 17:01:33 +0800

    #增加公共方法备注

--- src/service/public.js
+++ src/service/public.js
@@ -1,7 +1,11 @@
 const publics = {
+
+    /* 初始化表单值 */
     initFormList(fieldsList = []) {
         fieldsList.map(v => v.value = v.originValue);
     },
+
+    /* 绑定表单值 */
     bindFormData(item = {}, fieldsList = []) {
         fieldsList.forEach((v, n) => v.value = item[v.name]);
     }
