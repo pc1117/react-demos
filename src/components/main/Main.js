c83afc5cf3f20f26bd261100bfdb651d4bef6323
pc1117 <1023861544@qq.com>
2019-01-04 20:30:01 +0800

    #

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -78,7 +78,7 @@ export class Main extends Component {
     { name: "Name", displayName: "姓名", editor: "normal", value: "", originValue: "王小媛", rules: [{ required: true, message: "请输入姓名" }] },
     { name: "HouseGradePathName", displayName: "楼栋单元", editor: "select", value: "", originValue: 1, opts: [{ Id: 1, Name: "一单元" }, { Id: 2, Name: "二单元" }] },
     { name: "Sex", displayName: "性别", opts: [{ Id: 1, Name: "男" }, { Id: 2, Name: "女" }], editor: "radio", value: "", originValue: 2 },
-    { name: "Aihao", displayName: "民族", opts: [{ Id: 1, Name: "汉族" }, { Id: 2, Name: "其他少数民族" }], editor: "checkbox", value: "", originValue: [1], rules: [{ required: true, message: "请选择民族" }] },
+    { name: "Aihao", displayName: "民族", opts: [{ Id: 1, Name: "汉族" }, { Id: 2, Name: "其他少数民族" }], editor: "radio", value: "", originValue: 1, rules: [{ required: true, message: "请选择民族" }] },
     { name: "Phone", displayName: "手机", editor: "normal", value: "", originValue: "", rules: [{ required: false, message: "请输入手机" }] }];
 
     /* 钩子函数 */
--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,60 +1,53 @@
 import Mock from 'mockjs';
 
-const mainDataSource = () => {
-    let pagelist = [];
-    for (let i = 0; i < 10; i++) {
-        pagelist.push(Mock.mock({
-            'Id': i + 1,
-            'Email': '@EMAIL',
-            "Name": "@NAME",
-            "Sex|1-2": 1,
-            "City": "@CITY"
-        }))
-    }
-    return {
-        "State": {
-            "Code": 0,
-            "Message": "success"
+const mainDataSource = {
+    "State": {
+        "Code": 0,
+        "Message": "success"
+    },
+    "Action": null,
+    "Content": {
+        "paginator": {
+            "length": 0,
+            "totleNum": 0,
+            "currentPage": 1,
+            "totlePages": 1
         },
-        "Action": null,
-        "Content": {
-            "paginator": {
-                "length": 0,
-                "totleNum": 0,
-                "currentPage": 1,
-                "totlePages": 1
-            },
-            pagelist
-        },
-        "length": 10
-    }
+        ...Mock.mock({
+            'pagelist|10': [{
+                'Id|+1': 1,
+                'Email': '@EMAIL',
+                "Name": "@NAME",
+                "Sex|1-2": 1,
+                "City": "@CITY"
+            }]
+        })
+    },
+    "length": 10
 }
 
-const conpanyDataSource = () => {
-    let pagelist = [];
-    for (let i = 0; i < 5; i++) {
-        pagelist.push(Mock.mock({
-            'Id': i + 1,
-            "Name": "@CITY"
-        }))
-    }
-    return {
-        "State": {
-            "Code": 0,
-            "Message": "success"
+const conpanyDataSource = {
+    "State": {
+        "Code": 0,
+        "Message": "success"
+    },
+    "Action": null,
+    "Content": {
+        "paginator": {
+            "length": 0,
+            "totleNum": 0,
+            "currentPage": 1,
+            "totlePages": 1
         },
-        "Action": null,
-        "Content": {
-            "paginator": {
-                "length": 0,
-                "totleNum": 0,
-                "currentPage": 1,
-                "totlePages": 1
-            },
-            pagelist
-        },
-        "length": 10
-    }
+        ...Mock.mock({
+            'pagelist|1-5': [{
+                'Id|+1': 1,
+                "Name": "@CITY"
+            }]
+        })
+    },
+    "length": 10
 }
+
 Mock.mock("/api/courtyardlist", 'get', mainDataSource);
 Mock.mock("/api/companylist?name=&currentPage=1&length=5", 'get', conpanyDataSource);
